658412967430f29ba2252fecbd842c4ba5e09104
Nina Weber <weber_nina@web.de>
2024-04-19 09:19:09 +0200

    delete projects

--- index.js
+++ index.js
@@ -16,31 +16,3 @@ progressBar.style.width = calculateScrollPercentage();
 document.addEventListener("scroll", () => {
   progressBar.style.width = calculateScrollPercentage();
 });
-
-// slider functions ----------------------------------------------------
-let slideIndex = 0;
-showSlide(slideIndex);
-
-function prevSlide() {
-  showSlide((slideIndex -= 1));
-}
-
-function nextSlide() {
-  showSlide((slideIndex += 1));
-}
-
-function showSlide(index) {
-  const slides = document.querySelectorAll(".project-slides img");
-
-  if (index < 0) {
-    slideIndex = slides.length - 1;
-  } else if (index >= slides.length) {
-    slideIndex = 0;
-  }
-
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-
-  slides[slideIndex].style.display = "block";
-}
