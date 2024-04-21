f6f089b2c67704e21f8a7b196c7da344754baff4
Nina Weber <weber_nina@web.de>
2024-04-21 16:35:42 +0200

    draft carousel html, js, css

--- index.js
+++ index.js
@@ -18,29 +18,70 @@ document.addEventListener("scroll", () => {
 });
 
 // slider functions ——————————————————————————
-let slideIndex = 0;
-showSlide(slideIndex);
+document.addEventListener("DOMContentLoaded", function () {
+  let carousel = document.getElementById("projectsCarousel");
+  let slides = carousel.getElementsByClassName("carousel-item");
+  let totalSlides = slides.length;
+  let currentIndex = 0;
 
-function prevSlide() {
-  showSlide((slideIndex -= 1));
-}
-
-function nextSlide() {
-  showSlide((slideIndex += 1));
-}
-
-function showSlide(index) {
-  const slides = document.querySelectorAll(".project-slides img");
-
-  if (index < 0) {
-    slideIndex = slides.length - 1;
-  } else if (index >= slides.length) {
-    slideIndex = 0;
+  function showSlide(index) {
+    for (let i = 0; i < slides.length; i++) {
+      slides[i].classList.remove("active");
+    }
+    slides[index].classList.add("active");
   }
 
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+  function nextSlide() {
+    currentIndex = (currentIndex + 1) % totalSlides;
+    showSlide(currentIndex);
   }
 
-  slides[slideIndex].style.display = "block";
-}
+  function previousSlide() {
+    currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
+    showSlide(currentIndex);
+  }
+
+  // Show first slide
+  showSlide(currentIndex);
+
+  // Previous slide
+  document
+    .querySelector(".carousel-control-previous")
+    .addEventListener("click", function () {
+      previousSlide();
+    });
+
+  // Next slide
+  document
+    .querySelector(".carousel-control-next")
+    .addEventListener("click", function () {
+      nextSlide();
+    });
+});
+
+// let slideIndex = 0;
+// showSlide(slideIndex);
+
+// function prevSlide() {
+//   showSlide((slideIndex -= 1));
+// }
+
+// function nextSlide() {
+//   showSlide((slideIndex += 1));
+// }
+
+// function showSlide(index) {
+//   const slides = document.querySelectorAll(".project-slides img");
+
+//   if (index < 0) {
+//     slideIndex = slides.length - 1;
+//   } else if (index >= slides.length) {
+//     slideIndex = 0;
+//   }
+
+//   for (let i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+
+//   slides[slideIndex].style.display = "block";
+// }
