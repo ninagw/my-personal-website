e61ebce2564216b0484d376c71afc4754f884f2a
Nina Weber <weber_nina@web.de>
2024-04-22 20:19:05 +0200

    implement carousel indivators

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Implement Progress Bar ---------------------------------------------
+// Progress Bar ——————————————————————————
 
 const progressBar = document.querySelector('[data-js="progress-bar"]');
 
@@ -17,18 +17,21 @@ document.addEventListener("scroll", () => {
   progressBar.style.width = calculateScrollPercentage();
 });
 
-// slider functions ——————————————————————————
-document.addEventListener("DOMContentLoaded", function () {
+// Carousel ——————————————————————————
+document.addEventListener("DOMContentLoaded", () => {
   let carousel = document.getElementById("projectsCarousel");
   let slides = carousel.getElementsByClassName("carousel-item");
+  let indicators = carousel.getElementsByClassName("carousel-indicator");
   let totalSlides = slides.length;
   let currentIndex = 0;
 
   function showSlide(index) {
     for (let i = 0; i < slides.length; i++) {
       slides[i].classList.remove("active");
+      indicators[i].classList.remove("active");
     }
     slides[index].classList.add("active");
+    indicators[index].classList.add("active");
   }
 
   function nextSlide() {
@@ -58,30 +61,3 @@ document.addEventListener("DOMContentLoaded", function () {
       nextSlide();
     });
 });
-
-// let slideIndex = 0;
-// showSlide(slideIndex);
-
-// function previousSlide() {
-//   showSlide((slideIndex -= 1));
-// }
-
-// function nextSlide() {
-//   showSlide((slideIndex += 1));
-// }
-
-// function showSlide(index) {
-//   const slides = document.querySelectorAll(".carousel-item img");
-
-//   if (index < 0) {
-//     slideIndex = slides.length - 1;
-//   } else if (index >= slides.length) {
-//     slideIndex = 0;
-//   }
-
-//   for (let i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none";
-//   }
-
-//   slides[slideIndex].style.display = "block";
-// }
